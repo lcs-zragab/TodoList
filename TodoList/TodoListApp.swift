55aabf318310c0a743a8172c916795d8281ef83e
zaid ragab <zragab@lcs.on.ca>
2023-04-03 15:12:05 -0400

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Zaid Ragab on 2023-04-03.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
